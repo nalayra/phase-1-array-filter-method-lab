52bd340f92322b00966c83c0fb24a1252116ec66
Nicholas Alayra <nalayra@hotmail.com>
2021-11-24 10:18:22 -0800

    commit tree bc2c6c21ff03c84081e8a53c13da6efe5f975bd9

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+// function findMatching(driver, name) {
+//     return driver.filter(doesItMatch =>
+//         doesItMatch.toLowerCase() === name.toLowerCase()
+//     )
+// }
+function findMatching(driver, name) {
+    return driver.filter(function (doesItMatch) {
+        return doesItMatch.toLowerCase() === name.toLowerCase();
+    })
+}
+function fuzzyMatch(driver, letters) {
+    return driver.filter(doesItMatch => doesItMatch.toLowerCase().indexOf(letters.toLowerCase()) === 0);
+}
+
+function matchName(driver, person) {
+    return driver.filter(doesItMatch => doesItMatch.name.toLowerCase() === person.toLowerCase());
+}
